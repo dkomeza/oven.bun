d7193c7f72a5a7aed4339839218aa4d63ee9f3f5
Dawid Komęza <dawid.komeza@gmail.com>
2023-11-10 11:38:44 +0000

    :sparkles: Add stop method to Bun server

--- src/oven.ts
+++ src/oven.ts
@@ -1,23 +1,26 @@
 import { Server } from "bun";
 
 interface OvenInterface {
-    port?: number;
-
+  port?: number;
 }
 
 class Oven {
-    private server: Server;
+  private server: Server;
 
-    constructor(params: OvenInterface = {}) {
-        this.server = Bun.serve({
-            port: params.port || 5000,
-            fetch: this.handle
-        })
-    }
+  constructor(params: OvenInterface = {}) {
+    this.server = Bun.serve({
+      port: params.port || 5000,
+      fetch: this.handle,
+    });
+  }
 
-    private async handle(request: Request, server: Server): Promise<Response> {
-        return new Response("Hello via Bun!");
-    }
+  public stop(): void {
+    this.server.stop();
+  }
+
+  private async handle(request: Request, server: Server): Promise<Response> {
+    return new Response("Hello via Bun!");
+  }
 }
 
 export default Oven;
