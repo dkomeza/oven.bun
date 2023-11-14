5c2bf50509ca1e267f4b5b1e4757788f86fbd8ac
Dawid Komęza <dawid.komeza@gmail.com>
2023-11-14 20:30:08 +0000

    :rotating_light: Add more GET tests

--- tests/get.test.ts
+++ tests/get.test.ts
@@ -12,4 +12,51 @@ test("Should handle GET requests", async () => {
   const text = await response.text();
 
   expect(text).toBe("Hello World!");
+
+  oven.stop();
+});
+
+test("Should handle GET requests with parameters", async () => {
+  const oven = new Oven();
+
+  oven.get("/:name", (req) => {
+    return new Response(`Hello World!`);
+  });
+
+  const response = await fetch("http://localhost:5000/World");
+  const text = await response.text();
+
+  expect(text).toBe("Hello World!");
+
+  oven.stop();
+});
+
+test("Should handle GET requests with wildcards", async () => {
+  const oven = new Oven();
+
+  oven.get("/hello/*", (req) => {
+    return new Response(`Hello World!`);
+  });
+
+  const response = await fetch("http://localhost:5000/hello/world");
+  const text = await response.text();
+
+  expect(text).toBe("Hello World!");
+
+  oven.stop();
+});
+
+test("Should throw a 404 on non-existing routes", async () => {
+  const oven = new Oven();
+
+  oven.get("/", (req) => {
+    return new Response("Hello World!");
+  });
+
+  const response = await fetch("http://localhost:5000/404");
+  const text = await response.text();
+
+  expect(text).toBe("Not found");
+
+  oven.stop();
 });
